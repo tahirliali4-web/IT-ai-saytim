8d0b866fea9b1dcc1d24478d688b40c7974b401a
tahirliali4-web <tahirliali4@gmail.com>
2026-06-19 16:58:14 +0400

    Update ai_kod.py

--- ai_kod.py
+++ ai_kod.py
@@ -1,19 +1,19 @@
+import streamlit as st
 from sklearn.tree import DecisionTreeClassifier
 
-# Süni intellekt üçün məlumatlar [Xəta Kodu, Port]
+# Səni məlumatların
 X_data = [[10, 80], [12, 443], [5, 21], [500, 0], [550, 0], [600, 12]]
-y_cavablar = [0, 0, 0, 1, 1, 1]  # 0 = Şəbəkə, 1 = Sistem
+y_cavablar = [0, 0, 0, 1, 1, 1] 
 
 model = DecisionTreeClassifier()
 model.fit(X_data, y_cavablar)
 
-print("Suni intellekt oyrendi!\n")
-
 # Yeni problemi yoxlayırıq
-yeni_problem = [[8, 80]] 
-texmin = model.predict(yeni_problem)
+yeni_problem = [8, 80]
+texmin = model.predict([yeni_problem])
 
+# Ekranda göstərmək üçün st.write istifadə edirik
 if texmin[0] == 0:
-    print(f"Giris {yeni_problem} ucun AI Texmini: Bu bir SEBEKE xetasidir!")
+    st.write(f"Giris {yeni_problem} ucun AI Texmini: Bu bir SEBEKE xetasidir!")
 else:
-    print(f"Giris {yeni_problem} ucun AI Texmini: Bu bir SISTEM xetasidir!")
+    st.write(f"Giris {yeni_problem} ucun AI Texmini: Bu bir SISTEM xetasidir!")
